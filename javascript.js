76eccd3319fc092e4497b7759ca8da9b084f3d46
Matthieu L <llechmatthieu@gmail.com>
2026-09-21 19:10:31 +0200

    fin EAS

--- javascript.js
+++ javascript.js
@@ -1,7 +1,25 @@
 const container = document.querySelector(".container");
+const ask = document.querySelector("button");
 
-for (i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  container.appendChild(div);
-  console.log(div);
-}
+ask.addEventListener("click", () => {
+  let anwser = prompt("test");
+  const size = 100 / anwser;
+
+  if (anwser >= 100) {
+    alert("You can't display so much squares");
+  } else {
+    container.innerHTML = "";
+    for (i = 0; i < anwser * anwser; i++) {
+      const div = document.createElement("div");
+      container.appendChild(div);
+      div.style.width = `${size}%`;
+      div.style.height = `calc(100% / ${anwser})`;
+      div.addEventListener("mouseover", () => {
+        div.style.background = "red";
+      });
+      div.addEventListener("mouseout", () => {
+        div.style.background = "";
+      });
+    }
+  }
+});
